c9e21a6b24dd8665b0f3dcd3988b485aa08cb7e4
arthurdev06 <arthurdev6@gmail.com>
2025-04-10 00:55:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# IA no Lab
+
+Um aplicativo feito por estudantes do SESI/SENAI Sobradinho para ajudar em atividades laboratorias, simplificando a ciência
+
+Estudantes: Arthur Rocha, Eduardo Luís e João Gabriel
---
+++ main.py
@@ -0,0 +1,46 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+
+class User(BaseModel):
+    name: str
+
+
+class Users(BaseModel):
+    users: List[User]
+
+
+app = FastAPI(debug=True)
+
+origins = [
+    "http://localhost:5173",
+    # Add more origins here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_db = {"users": []}
+
+
+@app.get("/users", response_model=Users)
+def get_users():
+    return Users(users=memory_db["users"])
+
+
+@app.post("/users")
+def add_users(user: User):
+    memory_db["users"].append(user)
+    return user
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
