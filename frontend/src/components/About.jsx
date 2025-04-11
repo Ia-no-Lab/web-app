b3f83e8f7c66c05cd180c518b1e90548281b05ce
arthurdev06 <arthurdev6@gmail.com>
2025-04-11 14:57:33 -0300

    change about and terms

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./About.css";
-import {Link } from "@chakra-ui/react"
-import { LuExternalLink } from "react-icons/lu"
 
 function About() {
   return (
     <div className="bigBoxAbout">
       <h1>Sobre</h1>
       <p>Ia no Lab é um website de código aberto feito por estudantes que visa simplificar a ciência por meio do uso de Inteligência Artificial.</p>
-      <h2>
-      <Link
-        variant="underline"
-        fontSize = "2xl"
-        href="https://github.com/Ia-no-Lab" 
-        color="#24201bff">Visite o código<LuExternalLink/></Link>
-      </h2>
-    </div>
+     </div>
   );
 }
 
--- frontend/src/components/Terms.jsx
+++ frontend/src/components/Terms.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Terms.css";
+import {Link } from "@chakra-ui/react"
+import { LuExternalLink } from "react-icons/lu"
 
 function Terms() {
   return (
@@ -8,6 +10,13 @@ function Terms() {
       <p>
         Ia no Lab é um website de código aberto, então o modo que usamos seus dados está mostrado de forma transparente. 
       </p>
+     <h2>
+      <Link
+        variant="underline"
+        fontSize = "2xl"
+        href="https://github.com/Ia-no-Lab" 
+        color="#24201bff">Visite o código<LuExternalLink/></Link>
+      </h2>
 
     </div>
   );
