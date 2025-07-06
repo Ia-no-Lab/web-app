0733afc40f1c87c30c752916cf82540f7f9b2fc7
Arthur Rocha <arthurdev6@gmail.com>
2025-07-05 22:42:33 -0300

    fix for deploy

--- web-code/app/(protected)/home/chat/[chatId]/page.tsx
+++ web-code/app/(protected)/home/chat/[chatId]/page.tsx
@@ -1,11 +1,5 @@
 import ChatProvider from '@/components/chat/ChatProvider'
 
-interface PageProps {
-  params: { chatId: string }
-}
-
-export default async function ChatPage({ params }: PageProps) {
-  const { chatId } = params
-
-  return <ChatProvider chatId={chatId} />
+export default async function ChatPage({ params }: { params: { chatId: string } }) {
+  return <ChatProvider chatId={params.chatId} />
 }
