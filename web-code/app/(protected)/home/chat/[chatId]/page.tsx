f7b240a3a019e458fc94815bad5e3b62d91ca7a9
Arthur Rocha <arthurdev6@gmail.com>
2025-07-24 18:27:29 -0300

    trynna fix

--- web-code/app/(protected)/home/chat/[chatId]/page.tsx
+++ web-code/app/(protected)/home/chat/[chatId]/page.tsx
@@ -1,5 +1,7 @@
 import ChatProvider from '@/components/chat/ChatProvider'
 
-export default async function ChatPage({ params }: { params: { chatId: string } }) {
-  return <ChatProvider chatId={params.chatId} />
+export default async function ChatPage({ params }: { params: Promise<{ chatId: string }> }) {
+  const { chatId } = await params;
+  
+  return <ChatProvider chatId={chatId} />
 }
