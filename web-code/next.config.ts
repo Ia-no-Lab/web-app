5ac9cc706447520468aad95f9adfbfc33bff418b
Arthur Rocha <arthurdev6@gmail.com>
2025-08-04 02:07:28 -0300

    fix for deploy

--- web-code/next.config.ts
+++ web-code/next.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
@@ -7,6 +9,7 @@ const nextConfig = {
   experimental: {
     typedRoutes: false,
   },
+  output: 'standalone',
   headers: async () => [
     {
       source: '/(.*)',
@@ -27,4 +30,8 @@ const nextConfig = {
   },
 }
 
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+ 
 export default nextConfig
